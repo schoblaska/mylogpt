74d5430463adad226732cb5ba57787f4eec965f8
Joey Schoblaska <joey.schoblaska@gmail.com>
2023-07-19 17:13:23 -0500

    update gpt client

--- lib/gpt_client.rb
+++ lib/gpt_client.rb
@@ -1,9 +1,12 @@
 class GPTClient
+  EMBEDDINGS_MODEL = "text-embedding-ada-002"
+  GEN_MODEL = "gpt-3.5-turbo"
+
   def initialize
     @client = OpenAI::Client.new(access_token: ENV["OPENAI_ACCESS_TOKEN"])
   end
 
-  def chat(message, add_prompt: false)
+  def chat(message, add_prompt: false, model: GEN_MODEL)
     Timeout.timeout(30) do
       message = [{ role: "user", content: message }] if message.is_a?(String)
       message = PROMPT + message if add_prompt
@@ -11,7 +14,7 @@ class GPTClient
       response =
         @client.chat(
           parameters: {
-            model: "gpt-3.5-turbo",
+            model: model,
             messages: message,
             temperature: 0.5
           }
@@ -19,5 +22,12 @@ class GPTClient
 
       response.dig("choices", 0, "message", "content")
     end
+  end
+
+  def embedding(input)
+    response =
+      @client.embeddings(parameters: { model: EMBEDDINGS_MODEL, input: input })
+
+    response.dig("data", 0, "embedding")
   end
 end
